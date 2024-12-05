931f0781af83faf20a55e3b08c2c0dacba17af04
teju2707 <tejaswiniprasadwakte@gmail.com>
2024-12-05 23:11:25 +0530

    S3 bucket with DynamoDB for state locking.

--- terraform/backeend.tf
+++ terraform/backeend.tf
@@ -1,9 +1,11 @@
 terraform {
   backend "s3" {
+
     bucket         = "my-terraform-state"
     key            = "global/s3/terraform.tfstate"
     region         = "us-west-2"
     dynamodb_table = "terraform-lock"
     encrypt        = true
+
   }
 }
