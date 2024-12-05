1a6d81066d4228d5aba3fa5ad066275f83542510
teju2707 <tejaswiniprasadwakte@gmail.com>
2024-12-05 23:05:00 +0530

    define input parameters for your Terraform scripts, which are reusable and customizable per environment.

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,3 +1,17 @@
-output "vpc_id" {
-  value = module.vpc.vpc_id
+variable "aws_region" {
+  description = "The AWS region to deploy to"
+  type        = string
+  default     = "us-west-2"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "eks_cluster_name" {
+  description = "The name of the EKS cluster"
+  type        = string
+  default     = "my-eks-cluster"
 }
