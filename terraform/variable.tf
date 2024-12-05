dc2189fb7ee4ba57c6fe1c94bdd56afd5bf80e97
teju2707 <tejaswiniprasadwakte@gmail.com>
2024-12-05 23:05:32 +0530

    defines outputs that you can reference after Terraform applies the configuration. These could be the VPC ID, EKS cluster endpoint, etc., which might be useful for other modules or for your post-deployment actions.

--- terraform/variable.tf
+++ terraform/variable.tf
@@ -1,3 +1,14 @@
-variable "region" {
-  default = "us-east-1"
+output "vpc_id" {
+  description = "ID of the created VPC"
+  value       = module.vpc.vpc_id
+}
+
+output "eks_cluster_endpoint" {
+  description = "The endpoint for the EKS cluster"
+  value       = module.eks.cluster_endpoint
+}
+
+output "eks_cluster_name" {
+  description = "The name of the EKS cluster"
+  value       = module.eks.cluster_name
 }
