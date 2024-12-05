b5d35f34ff0733e37d65ee4b268ee595c3a75f7a
teju2707 <tejaswiniprasadwakte@gmail.com>
2024-12-05 23:08:43 +0530

    Create security-group.tf

---
+++ terraform/security-group.tf
@@ -0,0 +1,22 @@
+resource "aws_security_group" "eks_sg" {
+  name_prefix = "eks-cluster-sg"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress {
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "eks-cluster-sg"
+  }
+}
