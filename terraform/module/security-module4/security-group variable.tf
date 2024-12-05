b1ce2a4cfa7f38f1793275ecc15a15646a0359d4
teju2707 <tejaswiniprasadwakte@gmail.com>
2024-12-05 23:28:15 +0530

    Create security-group variable.tf

---
+++ terraform/module/security-module4/security-group variable.tf
@@ -0,0 +1,23 @@
+resource "aws_security_group" "eks_sg" {
+  name        = var.sg_name
+  description = "EKS security group"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    "Name" = var.sg_name
+  }
+}
